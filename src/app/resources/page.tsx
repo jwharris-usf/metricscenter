2ee4b2041870d0be86d9c2e876190ea36d1e9b44
Jeff <jeff@MacBook-Pro.local>
2024-06-01 21:24:10 -0400

    WEB040 - trying to fix stretched image on mobile only

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -36,9 +36,9 @@ export default async function Resources() {
             <div className="rounded-3xl border border-gray-100 bg-white shadow-2xl shadow-gray-600/25">
               <div className="flex flex-col md:flex-row">
                 <div className="md:w-1/2 lg:w-1/3 p-2">
-                  <div className="overflow-hidden border rounded-xl h-full">
+                  <div className="overflow-hidden border rounded-xl md:h-full">
                     <a href="https://youtu.be/yO2MT3-kLYY?si=loWtxkfewlpmbdC4" target="_blank">
-                      <img className="mx-auto h-full rounded-xl object-cover object-center hover:scale-110 duration-500" src="images/MTSS-Podcast.png" alt="Reimagining MTSS: A Pathway to Whole School Well-Being" loading="lazy" width="" height=""/>
+                      <img className="mx-auto w-full h-[18em] md:h-full rounded-xl object-cover object-center hover:scale-110 duration-500" src="images/MTSS-Podcast.png" alt="Reimagining MTSS: A Pathway to Whole School Well-Being" loading="lazy" width="" height=""/>
                     </a>
                   </div>
                 </div>
@@ -53,14 +53,14 @@ export default async function Resources() {
                       <p className="p-4">Hosted by Kari Vogelgesang, Michael Furlong and Erin Dowdy delve into MTSS (Multi-Tiered System of Supports) and its impact on students&apos; holistic development, encouraging educators to pause and reconsider the WHY behind MTSS, and inspire them to reflect on their practice, embrace a student-centered approach, and champion the journey of self-determination for every student.</p>
                     </div>
                   </div>
-                  <div className="pl-4 pt-2 pb-6 flex items-center">
+                  <div className="pl-4 pt-2 pb-6 flex flex-col lg:items-center lg:flex-row">
                     <div>
                       <p className="pl-4 font-bold">
                         <a className="flex pb-2" href="https://education.ucsb.edu/people/dean-emeriti-faculty/michael-furlong" target="_blank">Michael Furlong <RxOpenInNewWindow size={18} className="ml-1 relative top-[2px]"/></a>
                         <a className="flex" href="https://education.ucsb.edu/research-faculty/bio?first=Erin&last=Dowdy" target="_blank">Erin Dowdy <RxOpenInNewWindow size={18} className="ml-1 relative top-[2px]"/></a>
                       </p>
                     </div>
-                    <div className="flex grow justify-end pr-8 md:pr-6">
+                    <div className="flex grow pl-4 pt-4 pr-8 lg:pr-6 lg:pt-0 lg:justify-end">
 {/*
                       <a href="https://scsmh.education.uiowa.edu/wp-content/uploads/2024/05/K-12-Suicide-Prevention-Practice-Brief-4.pdf" target="_blank">
                         <Button variant="outline">
